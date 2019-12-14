0826689152d84126758a214613e02f8c6d140d78
Christopher Steffen <haxys@Christophers-MacBook-Air.local>
2019-12-14 14:37:30 -0600

    Standardize input.

--- cryptz.py
+++ cryptz.py
@@ -46,6 +46,22 @@ made by: {Fore.RED}r2dr0dn{Style.RESET_ALL}
 MENU_OPTIONS = list()
 
 
+def get_input(color, message):
+    return input(color + message + Style.RESET_ALL).encode()
+
+
+def get_plaintext():
+    return get_input(Fore.GREEN, "Enter plaintext message: ")
+
+
+def get_encoded():
+    return get_input(Fore.YELLOW, "Enter encoded message: ")
+
+
+def get_password():
+    return get_input(Fore.RED, "Enter encryption password: ")
+
+
 def ran_generator():
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
     size = 4
@@ -55,8 +71,7 @@ def ran_generator():
 
 def hex_enc():
     """Encode to Hexadecimal."""
-    data = input(Fore.RED + "Enter Your Plain Text Message: ")
-    data = data.encode()
+    data = get_plaintext()
     enc = binascii.hexlify(data)
     enc = enc.decode()
     print("\n" + Fore.RED + enc + Style.RESET_ALL + "\n")
@@ -67,8 +82,7 @@ MENU_OPTIONS.append(hex_enc)
 
 def hex_dec():
     """Decode from Hexadecimal."""
-    data = input(Fore.RED + "Enter Your Encrypted Form: ")
-    data = data.encode()
+    data = get_encoded()
     dec = binascii.unhexlify(data)
     dec = dec.decode()
     print("\n" + Fore.RED + dec + Style.RESET_ALL + "\n")
@@ -78,11 +92,8 @@ MENU_OPTIONS.append(hex_dec)
 
 
 def binhex4_enc():
-    """Encrypt with BinHex4."""
-    data = input(
-        Fore.GREEN + "Enter Your Plain Text Message: " + Style.RESET_ALL
-    )
-    data = data.encode()
+    """Encode with BinHex4."""
+    data = get_plaintext()
     enc = binascii.b2a_hqx(data)
     enc = enc.decode()
     print("\n" + Fore.CYAN + enc + Style.RESET_ALL + "\n")
@@ -94,10 +105,8 @@ def binhex4_enc():
 
 # TODO: Fix this code.
 # def binhex4_dec():
-#     """Decrypt with BinHex4."""
-#     data = input(Fore.RED + "Enter Your Encrypted Form: ")
-#     data = data.encode()
-#     # data = '-6)c0M8h1$PaGf9PFR)'
+#     """Decode with BinHex4."""
+#     data = get_encoded()
 #     dec = binascii.a2b_hqx(data)
 #     dec = dec.decode()
 #     print("\n" + Fore.MAGENTA + Style.DIM + dec + Style.RESET_ALL + "\n")
@@ -106,8 +115,7 @@ def binhex4_enc():
 
 def fernet_enc():
     """Encrypt with Fernet. (Symmetric)"""
-    data = input(Fore.RED + "Enter Your Plain Text Message: ")
-    data = data.encode()
+    data = get_plaintext()
     key = Fernet.generate_key()
     e = Fernet(key)
     encry = e.encrypt(data)
@@ -122,10 +130,8 @@ MENU_OPTIONS.append(fernet_enc)
 
 def fernet_dec():
     """Decrypt with Fernet. (Symmetric)"""
-    password = input(Fore.RED + "Enter Decryption Password: ")
-    password = password.encode()
-    encr = input(Fore.GREEN + "Enter Encryption Value: ")
-    encr = encr.encode()
+    encr = get_encoded()
+    password = get_password()
     D = Fernet(password)
     decr = D.decrypt(encr)
     decr = decr.decode()
@@ -137,8 +143,7 @@ MENU_OPTIONS.append(fernet_dec)
 
 def uu_enc():
     """Encode with uuencode."""
-    data = input(Fore.RED + "Enter Your Plain Text Message: ")
-    data = data.encode()
+    data = get_plaintext()
     enc = binascii.b2a_uu(data)
     enc = enc.encode()
     print("\n" + Fore.RED + enc + Style.RESET_ALL + "\n")
@@ -149,7 +154,7 @@ MENU_OPTIONS.append(uu_enc)
 
 def uu_dec():
     """Decode with uudecode."""
-    data = input(Fore.RED + "Enter Your Encrypted Form: ")
+    data = get_encoded()
     dec = binascii.a2b_uu(data)
     dec = dec.encode()
     print("\n" + Fore.RED + dec + Style.RESET_ALL + "\n")
@@ -160,7 +165,7 @@ MENU_OPTIONS.append(uu_dec)
 
 def base64_enc():
     """Encode with Base64."""
-    data = input(Fore.MAGENTA + "Enter Your Plain Text Message: ").encode()
+    data = get_plaintext()
     enc = pybase64._pybase64.b64encode(data).decode()
     print("\n" + Fore.YELLOW + enc + Style.RESET_ALL + "\n")
 
@@ -170,7 +175,7 @@ MENU_OPTIONS.append(base64_enc)
 
 def base64_dec():
     """Decode with Base64."""
-    data = input(Fore.RED + "Enter Your Encrypted Form: ").encode()
+    data = get_encoded()
     dec = pybase64._pybase64.b64decode(data).decode()
     print("\n" + Fore.RED + dec + Style.RESET_ALL + "\n")
 
@@ -182,8 +187,7 @@ def aes_enc_manual():
     """Encrypt with AES. (Manual)"""
     keypass = ran_generator()
     keypass2 = keypass
-    data = input(Fore.MAGENTA + "Enter Your Plain Text Message: ")
-    data = data.encode()
+    data = get_plaintext()
     keypass = keypass.encode()
     cipher = AES.new(keypass, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(data)
@@ -206,8 +210,7 @@ def aes_enc_manual():
 # def aes_dec_manual():
 #     """Decrypt with AES. (Manual)"""
 #     try:
-#         keypass = input(Fore.RED + "Enter Your Decryption Password: ")
-#         keypass = keypass.encode()
+#         keypass = get_password()
 #         tag = input('Enter Tag: ')
 #         # tag = tag.encode()
 #         tag = str.encode(tag)
@@ -229,7 +232,7 @@ def aes_enc_manual():
 
 def rsa_enc_manual():
     """Encrypt with RSA. (Manual)"""
-    data = input(Fore.RED + Style.BRIGHT + "Enter Your Plain Text Message: ")
+    data = get_plaintext()
     BLOCK_SIZE = 16
     PADDING = "{"
     pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
@@ -253,10 +256,10 @@ def rsa_enc_manual():
 
 def aes_enc_auto():
     """Encrypt with AES. (Automatic)"""
-    data = input(Fore.MAGENTA + "Enter Your Plain Text Message: ")
+    data = get_plaintext()
     data = data.encode()
-    filename = input(
-        Fore.YELLOW + "Enter FileName To Encrypted Data Be Saved In: "
+    filename = get_input(
+        Fore.YELLOW, "Enter output filename: "
     )
     keypass = ran_generator()
     keypass2 = keypass
@@ -299,8 +302,7 @@ def aes_dec_auto():
         + "Enter Encrypted Data File (make sure it on the same path): "
     )
     file_in = open(filename, "rb")
-    keypass = input(Fore.RED + "Enter Decryption Password: ")
-    keypass = keypass.encode()
+    keypass = get_password()
     nonce, tag, ciphertext = [file_in.read(x) for x in (16, 16, -1)]
     cipher = AES.new(keypass, AES.MODE_EAX, nonce)
     data = cipher.decrypt_and_verify(ciphertext, tag)
@@ -325,7 +327,7 @@ def main():
                     f"{index + 1}. {' ' if index < 9 else ''}"
                     f"{MENU_OPTIONS[index].__doc__}"
                 )
-            choice = input(Fore.CYAN + "CRYPTZ -> " + Style.RESET_ALL)
+            choice = get_input(Fore.CYAN, "CRYPTZ -> ")
             try:
                 MENU_OPTIONS[int(choice) - 1]()
             except IndexError:
